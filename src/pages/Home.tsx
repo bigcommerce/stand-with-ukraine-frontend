0762cf5f3dc41624f0ebfd0891c0fd354b752661
BC Micah <micah.thomas@bigcommerce.com>
2022-04-26 16:23:55 -0600

    Get store status on load

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,21 +3,25 @@ import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Content from '../components/Home/Content';
 import Header from '../components/Home/Header';
-import { selectHome } from '../components/Home/homeSlice';
+import { loadStatus, selectHome } from '../components/Home/homeSlice';
 import NotPublishedActions from '../components/Home/NotPublishedActions';
 import PublishedActions from '../components/Home/PublishedActions';
-import { useAppSelector } from '../state/hooks';
+import { useAppDispatch, useAppSelector } from '../state/hooks';
+import { SetSessionToken } from '../utils';
 
 export default function Home() {
   const { published } = useAppSelector(selectHome);
+  const dispatch = useAppDispatch();
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
     const token = searchParams.get('token');
     if (token && token.split('.').length === 3) {
-      sessionStorage.setItem('app-bearer-token', token);
+      SetSessionToken(token);
     }
-  }, [searchParams]);
+
+    dispatch(loadStatus());
+  }, [searchParams, dispatch]);
 
   return (
     <>
