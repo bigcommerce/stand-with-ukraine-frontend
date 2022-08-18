ef848a51bc628182ba2877362d32c8c8b2ec7cb1
Stanislav Holts <stanislav.holts@bigcommerce.com>
2022-08-18 13:13:37 +0300

    chore(configs): add commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'all',
+        'ci',
+        'widget',
+        'dashboard',
+        'configs',
+        'deps',
+        'release',
+        'mocks',
+      ],
+    ],
+  },
+};
