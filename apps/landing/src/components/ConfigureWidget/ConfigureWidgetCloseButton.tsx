af0b58668a7a563cf79ffcae66c5152c4d880028
Micah Thomas <micah.thomas@bigcommerce.com>
2023-01-10 07:44:11 -0700

    fix: make mobile iframe fullscreen and fix overlay z-index

--- apps/landing/src/components/ConfigureWidget/ConfigureWidgetCloseButton.tsx
+++ apps/landing/src/components/ConfigureWidget/ConfigureWidgetCloseButton.tsx
@@ -1,14 +1,36 @@
 import styled from 'styled-components';
+import { breakpoints } from '../../helpers';
 
 import CloseSVG from './close-button.svg';
 import { ConfigureWidgetContext } from './ConfigureWidgetContext';
 
 const CloseButton = styled.a`
   position: fixed;
-  right: 1.5rem;
-  top: calc(15% + 1.5rem);
+  right: 1rem;
+  top: 4.5rem;
   z-index: 11001;
   cursor: pointer;
+  height: 2rem;
+  width: 2rem;
+
+  & img {
+    height: 100%;
+    width: 100%;
+  }
+
+  ${breakpoints.tablet} and (orientation: portrait) {
+    top: calc(15% + 1.25rem);
+    right: 3rem;
+    height: 2.5rem;
+    width: 2.5rem;
+  }
+
+  ${breakpoints.desktop} {
+    top: calc(15% + 1.25rem);
+    right: 3rem;
+    height: 2.5rem;
+    width: 2.5rem;
+  }
 `;
 
 export default function ConfigureWidgetCloseButton() {
